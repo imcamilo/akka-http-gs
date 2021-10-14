f0ab3217ae07bdc71c4978b26cbcd5e0c1a45847
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2021-10-13 21:27:40 -0300

    chore(playground): add http request

--- src/main/scala/com/github/imcamilo/AkkaHttpPlayground.scala
+++ src/main/scala/com/github/imcamilo/AkkaHttpPlayground.scala
@@ -1,6 +1,39 @@
 package com.github.imcamilo
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest}
+import akka.http.scaladsl.server.ContentNegotiator.Alternative.ContentType
+import akka.stream.ActorMaterializer
+
+import java.net.URLEncoder
+
 object AkkaHttpPlayground {
+
+  implicit val system = ActorSystem() //akka actors
+  implicit val materializer = ActorMaterializer() //akka streams
+  import system.dispatcher //thread pool
+
+  val source =
+    """
+      |object SimpleApp {
+      |  val firstField = 2
+      |
+      |  def firstMethod(x: Int) = x + 1
+      |
+      |  def main(args: Array[String]): Unit = println(firstField)
+      |}
+      |""".stripMargin
+
+  val request = HttpRequest(
+    method = HttpMethods.POST,
+    uri = "http://markup.su/api/highlighter",
+    entity = HttpEntity(
+      ContentTypes.`application/x-www-form-urlencoded`,
+      s"source=${URLEncoder.encode(source.trim, "UTF-8")}&language=Scala&theme=Sunburst"
+    )
+  )
+
+  def sendRequest() = ???
 
   def main(args: Array[String]): Unit = {
 
